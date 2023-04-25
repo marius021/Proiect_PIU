4c07f6b2bd01bdf60060f9c9902a3fe6eebf9539
Maurice01 <marius.petreanu@student.unitbv.ro>
2023-04-25 19:17:35 +0300

    Update 25/04

---
+++ src/app/services.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Item } from 'src/models/items';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+   
+  constructor(public httpClient: HttpClient) { }
+  readonly baseUrl = 'http://localhost:44385/api/item/';
+
+  getItems(): Observable<Item[]> {
+    return this.httpClient.get<Item[]>(this.baseUrl + 'get') as Observable<Item[]>; // this.baseUrl + 'get' is the url of the api
+  }
+
+  getItemById(id: number): Observable<Item> {
+    return this.httpClient.get<Item>(this.baseUrl + 'getItems/' + id) as Observable<Item>;
+  }
+
+  createItem(item: Item): Observable<Item> {
+    return this.httpClient.post<Item>(this.baseUrl + 'post', item) as Observable<Item>;
+  }
+
+  delete(id: number): Observable<null> {
+    return this.httpClient.delete<Item>(this.baseUrl + 'delete/' + id) as unknown as Observable<null>;
+  }
+
+  editItem(item: Item): Observable<null> {
+    return this.httpClient.put(this.baseUrl + 'edit/'+item.id, item) as unknown as Observable<null>;
+  }
+}
